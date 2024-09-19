bcea0ef891cc758651a8f42ade46c8f8b0048e59
Damianexe <jortizt5@est.ups.edu.ec>
2024-09-18 20:48:44 -0500

    Practica final

--- src/App.java
+++ src/App.java
@@ -11,21 +11,26 @@ public class App {
         System.out.println(cadena.length());
         System.out.println(cadena.split(" ")[0]);
 
-        for ( int i = 0 ; i< cadena.length();i++ ){
-
-            
-            if (i == cadena.length()-1){
-
+        for (int i = 0; i < cadena.length(); i++) {
+            if (i == cadena.length() - 1) {
                 System.out.println(cadena.charAt(i));
-            }else {
-                    System.out.print(cadena.charAt(i)+"-");
-                }
+            } else {
+                System.out.print(cadena.charAt(i) + "-");
+            }
         }
 
-   //CONTAR PALABRAS      
-   int palabras=  cadena.split(" ").length;
-   System.out.println(palabras);
-   
-   //CONTAR CUANTAS "a" HAY EN LA CADENA
+        // CONTAR PALABRAS
+        int palabras = cadena.split(" ").length;
+        System.out.println("Cantidad de palabras: " + palabras);
+
+        // CONTAR CUANTAS "a" HAY EN LA CADENA
+        char letra = 'a';
+        int contador = 0;
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == letra) {
+                contador++;
+            }
+        }
+        System.out.println(+ contador);
     }
 }
